9ff71ac0bd3226d094fba54980d2b6d57ceaf10e
LinkunGao <gaolinkun123@gmail.com>
2024-06-13 19:00:59 +1200

    update load model function with image's position bias, update model

--- frontend/components/model/Model.vue
+++ frontend/components/model/Model.vue
@@ -59,7 +59,7 @@ export default {
   methods: {
     async start() {
       this.loadNrrd("modelView/breast_14.nrrd", "breastnrrd");
-      this.loadModel("modelView/prone_surface.obj");
+      
     },
 
     loadNrrd(nrrdUrl, modelName) {
@@ -80,6 +80,16 @@ export default {
           true,
           (volume, nrrdMesh, nrrdSlices, gui) => {
             this.scene.addObject(nrrdMesh.z);
+            const nrrdOrigin = volume.header.space_origin.map((num) => Number(num));
+            const nrrdRas = volume.RASDimensions; 
+    
+
+            const x_bias = -(nrrdOrigin[0] * 2 + nrrdRas[0]) / 2;
+            const y_bias = -(nrrdOrigin[1] * 2 + nrrdRas[1]) / 2;
+            const z_bias = -(nrrdOrigin[2] * 2 + nrrdRas[2]) / 2;
+
+            this.nrrdBias = new this.THREE.Vector3(x_bias, y_bias, z_bias);
+            this.loadModel("modelView/prone.obj");
           },
           { openGui: false }
         );
@@ -98,10 +108,10 @@ export default {
         const size = box.getSize(new this.THREE.Vector3()).length();
         const center = box.getCenter(new this.THREE.Vector3());
 
-        content.position.x += content.position.x - center.x;
-        content.position.y += content.position.y - center.y;
-        content.position.z += content.position.z - center.z;
-
+       // content.position.x += content.position.x - center.x;
+        //content.position.y += content.position.y - center.y;
+        //content.position.z += content.position.z - center.z;
+        content.position.set(this.nrrdBias.x, this.nrrdBias.y, this.nrrdBias.z);
         content.renderOrder = 3;
         content.traverse((child) => {
           if (child.isMesh) {
